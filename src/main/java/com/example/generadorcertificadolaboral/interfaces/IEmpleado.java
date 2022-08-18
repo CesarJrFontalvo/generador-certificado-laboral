237ce5d490f6948db43da1993d6614b1c533387c
Johan Gómez <jsganewcont@gmail.com>
2022-08-18 11:42:12 -0500

    Act comentario

--- src/main/java/com/example/generadorcertificadolaboral/interfaces/IEmpleado.java
+++ src/main/java/com/example/generadorcertificadolaboral/interfaces/IEmpleado.java
@@ -1,5 +1,12 @@
 package com.example.generadorcertificadolaboral.interfaces;
 
-public interface IEmpleado {
+import java.util.List;
 
+import com.example.generadorcertificadolaboral.models.EntityEmpleado;
+
+
+public interface IEmpleado {
+	public List<EntityEmpleado> findAll();
+	///Esto es un comentario para resubir el commit
+	
 }
