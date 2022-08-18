dfe09017f5d694e9a3b5fb80e116496bd65d2f26
Cesar Jr.Fontalvo <juniorfontalvo5@gmail.com>
2022-08-18 10:11:54 -0500

    first commit

---
+++ src/test/java/com/example/generadorcertificadolaboral/GeneradorCertificadoLaboralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.generadorcertificadolaboral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneradorCertificadoLaboralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
