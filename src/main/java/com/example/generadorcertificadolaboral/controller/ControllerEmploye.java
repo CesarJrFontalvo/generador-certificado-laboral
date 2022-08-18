9256c0652b0eceb934d8c56ffde259dc8bc34b97
Cesar Jr.Fontalvo <juniorfontalvo5@gmail.com>
2022-08-18 12:13:02 -0500

    fix: controller Empleado

---
+++ src/main/java/com/example/generadorcertificadolaboral/controller/ControllerEmploye.java
@@ -0,0 +1,10 @@
+package com.example.generadorcertificadolaboral.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerEmploye {
+    @GetMapping("/hola")
+    public String helloMundo() {return "Hola mundo que tal vamos? Hasta luego!";};
+}
