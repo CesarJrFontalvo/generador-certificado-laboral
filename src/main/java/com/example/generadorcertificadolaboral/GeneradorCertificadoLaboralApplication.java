4645a3581e1cbf9558edacadbe4078f7c9ef7931
Cesar Jr.Fontalvo <juniorfontalvo5@gmail.com>
2022-08-18 10:11:54 -0500

    fix: creacion de package Models con su clase entidad

---
+++ src/main/java/com/example/generadorcertificadolaboral/GeneradorCertificadoLaboralApplication.java
@@ -0,0 +1,13 @@
+package com.example.generadorcertificadolaboral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneradorCertificadoLaboralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneradorCertificadoLaboralApplication.class, args);
+	}
+
+}
